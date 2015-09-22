633bb6da30067cc5f9306e199f9f6c52acb35f9c
Administrator <libe4860@gmail.com>
2015-09-22 10:45:42 +0900

    append view .jsp

--- src/main/java/com/webapp/controller/AjaxController.java
+++ src/main/java/com/webapp/controller/AjaxController.java
@@ -2,9 +2,23 @@ package com.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/ajax")
 public class AjaxController {
+	
+	
+	//ajax관련된 view만 받아들임
+	@RequestMapping(value="/view", method=RequestMethod.GET)
+	public String getView() {
+		
+		return "ajax/view";
+	}
 
+	//ajax페이지를 통해서 요청한 데이터를 읽어들임
+	@RequestMapping(value="/array", method=RequestMethod.GET)
+	public void getArray() {
+		
+	}
 }
